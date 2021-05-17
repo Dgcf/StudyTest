dad5b0d3b276d9720df36634fa639b160f378f86
unknown <gongfengzztx@163.com>
2021-05-17 23:49:31 +0800

    update

--- DesignModel/DesignModel/SmartPtr.h
+++ DesignModel/DesignModel/SmartPtr.h
@@ -7,3 +7,39 @@ struct NoChecking
 {
 	static void Check(T*) {}
 };
+
+template<typename T>
+struct EnforceNotNull
+{
+	class NullPointerException: public std::exception{...};
+	static void Check(T* ptr)
+	{
+		if (!ptr)
+		{
+			throw NullPointException();
+		}
+	}
+};
+
+
+template<typename T>
+struct EnsureNotNull
+{
+	static void Check(T*& ptr)
+	{
+		if (!ptr)
+			ptr = GetDefaultValue();
+	}
+};
+
+
+template
+<
+	typename T,
+	template <typename> class CheckingPolicy,
+	template<typename> class ThreadingModel
+>
+class SmartPtr: public CheckingPolicy<T>, public ThreadingModel<SmartPtr>
+{
+
+};
