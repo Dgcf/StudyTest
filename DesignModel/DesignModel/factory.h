9f1187aa81ef53a74e36f4f00268844a1dc2ce66
unknown <gongfengzztx@163.com>
2021-05-26 23:24:28 +0800

    update

--- DesignModel/DesignModel/factory.h
+++ DesignModel/DesignModel/factory.h
@@ -54,5 +54,85 @@ typedef AbstractFactory<
 > AbstractEnemyFactory;
 
 
+class Shape
+{
+public:
+	virtual void Draw() const = 0;
+	virtual void Rotate(double angle) = 0;
+	virtual void Zoom(double zoomFactor) = 0;
+};
+
+class Drawing
+{
+
+};
+
+template
+<
+	typename AbstractProduct,
+	typename IdentifierType,
+	typename ProductCreator
+>
+class Factory
+{
+public:
+	bool Register(const IdentifierType& id, ProductCreator creator)
+	{
+		return associations_.insert(AssocMap::vlaue_type(id, creator)).second;
+	}
+
+	bool UnRegister(const IdentifierType& id)
+	{
+		return associations_.erase(id) == 1;
+	}
+
+	AbstractProduct* CreateObject(const IdentifierType& id)
+	{
+		typename AssocMap::const_iterator it = associations_.find(id);
+		if (it == associations_.end())
+		{
+			throw runtime_error("")
+		}
+		return (it->second)();
+	}
+
+private:
+	typedef std::map<IdentifierType, AbstractProduct> AssocMap;
+	AssocMap associations_;
+};
+
+
+class ShapeFactory
+{
+public:
+	typedef Shape* (*CreateShapeCallback)();
+
+private:
+	typedef std::map<int, CreateShapeCallback> CallbackMap;
+
+public:
+	bool RegisterShape(int ShapeId, CreateShapeCallback CreateFn)
+	{
+		return callbacks_.insert(CallbackMap::value_type(ShapeId, CreateFn)).second;
+	}
+
+	bool UnRegisterShape(int ShapeId)
+	{
+		return callbacks_.erase(ShapeId) == 1;
+	}
+
+	Shape* CreateShape(int ShapeId)
+	{
+		CallbackMap::const_iterator it = callbacks_.find(ShapeId);
+		if (it == callbacks_.end())
+		{
+			throw runtime_error("Unknown Shape ID");
+		}
+		return (it->second)();
+	}
+
+private:
+	CallbackMap callbacks_;
+};
 
 
